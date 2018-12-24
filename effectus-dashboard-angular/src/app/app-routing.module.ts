008d2a7887c5769d10b92304fcdb9e381c4f6824
gadi eichhorn <gadi.eichhorn@gmail.com>
2018-12-24 13:06:39 +0200

    add basic modules and components with bs4

--- effectus-dashboard-angular/src/app/app-routing.module.ts
+++ effectus-dashboard-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccountsComponent}from './effectus/accounts/accounts.component';
+import {CustomersComponent}from './effectus/customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'accounts', component: AccountsComponent},
+{path: 'customers',component: CustomersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- effectus-dashboard-angular/src/app/app.module.ts
+++ effectus-dashboard-angular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavbarComponent}from './navbar/navbar.component';
+import {SidenavComponent}from './sidenav/sidenav.component';
+import {FooterComponent}from './footer/footer.component';
+import {EffectusModule }from './effectus/effectus.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+declarations: [
+AppComponent,
+NavbarComponent,
+SidenavComponent,
+FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+AppRoutingModule,
+EffectusModule
   ],
   providers: [],
   bootstrap: [AppComponent]
