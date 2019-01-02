4b56d2ad4ef5d6a33624b819d6df5cf19ae3da1e
gadi eichhorn <gadi.eichhorn@gmail.com>
2019-01-02 09:44:24 +0200

    add customers to view

--- effectus-dashboard-angular/src/app/effectus/customers/customers.component.ts
+++ effectus-dashboard-angular/src/app/effectus/customers/customers.component.ts
@@ -1,4 +1,16 @@
 import{Component, OnInit}from '@angular/core';
+import gql from 'graphql-tag';
+import {QueryRef, Apollo}from 'apollo-angular';
+
+const CUSTOMERS_QUERY = gql`
+query customers($offset: Int) {
+    customers(offset: $offset, first: 10) {
+      key
+      account
+      name
+    }
+  }
+`;
 
 @Component({
 selector: 'app-customers',
@@ -7,9 +19,21 @@ styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
 
-constructor() { }
+  page = 0;
+  customers: any[] = [];
+  private query: QueryRef<any>;
+
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
+    this.query = this.apollo.watchQuery({
+      query: CUSTOMERS_QUERY,
+      variables: { offset: 10 * this.page }
+    });
+
+    this.query.valueChanges.subscribe(result => {
+      this.customers = result.data && result.data.customers;
+    });
   }
 
 }
