f0a4f0833dab1802117ddf4c98d2bbcaa05f1298
gadi eichhorn <gadi.eichhorn@gmail.com>
2019-01-02 13:19:29 +0200

    add new object types to collections

--- effectus-dashboard-angular/src/app/app-routing.module.ts
+++ effectus-dashboard-angular/src/app/app-routing.module.ts
@@ -1,17 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {AccountsComponent}from './effectus/accounts/accounts.component';
-import {CustomersComponent}from './effectus/customers/customers.component';
-import { DashboardComponent}from './effectus/dashboard/dashboard.component';
+import{NgModule}from"@angular/core";
+import {Routes, RouterModule}from "@angular/router";
+import {AccountsComponent}from "./effectus/accounts/accounts.component";
+import {CustomersComponent}from "./effectus/customers/customers.component";
+import { DashboardComponent}from "./effectus/dashboard/dashboard.component";
+import {TeamsComponent} from "./effectus/teams/teams.component";
+import {ProjectsComponent}from "./effectus/projects/projects.component";
+import {ResourcesComponent}from "./effectus/resources/resources.component";
+import {TasksComponent}from "./effectus/tasks/tasks.component";
+import { UsersComponent}from "./effectus/users/users.component";
+import {AssignmentsComponent} from "./effectus/assignments/assignments.component";
 
 const routes: Routes = [
-{path: 'dashboard', component: DashboardComponent},
-{path: 'accounts', component: AccountsComponent},
-{path: 'customers',component: CustomersComponent}
+{path: "dashboard", component: DashboardComponent},
+{path: "accounts", component: AccountsComponent },
+{path: "customers", component: CustomersComponent},
+{path: "teams", component: TeamsComponent},
+{path: "projects", component: ProjectsComponent},
+{path: "resources", component: ResourcesComponent},
+{path: "tasks", component: TasksComponent},
+{path: "assignments", component: AssignmentsComponent},
+{ path: "users", component: UsersComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- effectus-dashboard-angular/src/app/effectus/effectus.module.ts
+++ effectus-dashboard-angular/src/app/effectus/effectus.module.ts
@@ -1,18 +1,28 @@
-import{NgModule}from'@angular/core';
-import {CommonModule }from '@angular/common';
-import {AccountsComponent}from './accounts/accounts.component';
-import {CustomersComponent} from './customers/customers.component';
-import {DashboardComponent}from './dashboard/dashboard.component';
+import{NgModule}from"@angular/core";
+import {CommonModule }from "@angular/common";
+import {AccountsComponent}from "./accounts/accounts.component";
+import {CustomersComponent} from "./customers/customers.component";
+import {DashboardComponent}from "./dashboard/dashboard.component";
+import { TeamsComponent}from "./teams/teams.component";
+import {ProjectsComponent}from "./projects/projects.component";
+import {ResourcesComponent}from "./resources/resources.component";
+import {TasksComponent} from "./tasks/tasks.component";
+import {AssignmentsComponent}from "./assignments/assignments.component";
+import { UsersComponent}from "./users/users.component";
 
 @NgModule({
 declarations: [
 AccountsComponent,
 CustomersComponent,
-DashboardComponent
+DashboardComponent,
+TeamsComponent,
+ProjectsComponent,
+ResourcesComponent,
+TasksComponent,
+AssignmentsComponent,
+UsersComponent
 ],
-imports: [
-CommonModule
-]
+imports: [CommonModule]
 })
 export class EffectusModule {
 
